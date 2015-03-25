7187c0cf43bf6b89a897a7011b786eae2e671996
nickchu35 <nickchu35@gmail.com>
2015-03-25 22:06:59 +0800

    align half done

--- alignLogicalTry.m
+++ alignLogicalTry.m
@@ -1,10 +1,18 @@
-function shift = align(im1,im2)
-% transform to grayscale imgs
+im1 = imread('HDR_Photos/img1_6.JPG'); 
+im2 = imread('HDR_Photos/img1_7.JPG');
+im1 = imresize(im1,0.1);
+im2 = imresize(im2,0.1);
+% imshow(im1);
+% figure, imshow(im2);
 img_gray1 = rgb2gray(im1);
 img_gray2 = rgb2gray(im2);
+% figure, imshow(img_gray1);
+% figure, imshow(img_gray2);
 % transform matrix to vector, and get the median of both imgs
-th1 = median(double(img_gray1(:)));
-th2 = median(double(img_gray2(:)));
+% th1 = median(double(img_gray1(:)));
+% th2 = median(double(img_gray2(:)));
+disp(th1);
+disp(th2);
 % initialize MTB imgs
 mtb1 = zeros(size(img_gray1,1),size(img_gray1,2));
 mtb2 = zeros(size(img_gray2,1),size(img_gray2,2));
@@ -23,7 +31,5 @@ for i = 1 : size(img_gray1,1)
         end
     end
 end
-
-
-
-end
+% figure, imshow(mtb1);
+% figure, imshow(mtb2);
