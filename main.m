417fd37d20de1d54c86e54931d8d8cfa8382c333
nickchu35 <nickchu35@gmail.com>
2015-03-27 17:00:16 +0800

    working on MTB final

--- main.m
+++ main.m
@@ -1,4 +1,6 @@
  % main file of the project
+ clear;
+ clc;
  %% originally want to downsample first from 6000 x 4000 to 1500 x 1000 (1/4), but mind has changed
 %  path = 'HDR_Photos/img2_';
 %  sub = '.JPG';
@@ -10,7 +12,7 @@
 %      fs = [path int2str(i) small_s sub];
 %      imwrite(smallim, fs);
 %  end
- %% Read l photos and transform to gray images
+ %% Read in photos
  img1 = imread('HDR_Photos/img1_1.JPG'); 
  img2 = imread('HDR_Photos/img1_2.JPG');
  img3 = imread('HDR_Photos/img1_3.JPG'); 
