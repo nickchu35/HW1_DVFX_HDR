c308fa1845ad5e2fd3e61d3902e8260eb9406c13
bun919tw <bun919tw@gmail.com>
2015-04-08 01:23:55 +0800

    add tone mapping in report

--- main_tonemap.m
+++ main_tonemap.m
@@ -3,7 +3,8 @@
 %%  Tone mapping
  disp('Tone mapping!!!');
  disp('Loading EMap.mat......');
- EnergyMap = cell2mat(struct2cell(load('../EMap.mat')));
+ EnergyMap = cell2mat(struct2cell(load('EMap.mat')));
+ imshow('EnergyMap');
  disp('Loading EMap.mat finished.');
  tic;
  [L,result_G,result_L] = tonemap(EnergyMap);
